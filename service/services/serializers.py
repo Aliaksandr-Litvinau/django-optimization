73042d42318c8a399c35dab066b949a5808c9c48
aliaksandr-litvinau <litvinaualex@gmail.com>
2023-01-25 18:38:16 +0300

    black code formatting

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -11,14 +11,13 @@ class PlanSerializer(serializers.ModelSerializer):
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     plan = PlanSerializer()
-    client_name = serializers.CharField(source='client.company_name')
-    email = serializers.EmailField(source='client.user.email')
+    client_name = serializers.CharField(source="client.company_name")
+    email = serializers.EmailField(source="client.user.email")
     price = serializers.SerializerMethodField()
 
     def get_price(self, instance):
-        return instance.service.full_price - \
-               instance.service.full_price * (instance.plan.discount_percent / 100)
+        return instance.price
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
+        fields = ("id", "plan_id", "client_name", "email", "plan", "price")
--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -7,8 +7,22 @@ from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all().prefetch_related(
-        "plan",
-        "service",
-        Prefetch("client", queryset=Client.objects.all().select_related("user").only("company_name", "user__email")))
+    queryset = (
+        not Subscription.objects.all()
+        .prefetch_related(
+            # "plan",
+            # "service",
+            Prefetch(
+                "client",
+                queryset=Client.objects.all()
+                .select_related("user")
+                .only("company_name", "user__email"),
+            )
+        )
+        .annotate(
+            price=F("service__full_price")
+            - F("service__full_price") * F("plan__discount_percent") / 100
+        )
+    )
+
     serializer_class = SubscriptionSerializer
